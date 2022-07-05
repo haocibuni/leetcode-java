3a48e7f47e69174622169be91ebd7db8dd4597c0
haocibuni <1723541415@qq.com>
2022-07-05 22:00:27 +0800

    issubstructure

---
+++ 剑指 Offer 26. 树的子结构/src/isSubStructure.java
@@ -0,0 +1,11 @@
+import javax.swing.tree.TreeNode;
+
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode(int x) { val = x; } }
+ */
+class Solution {
+	public boolean isSubStructure(TreeNode A, TreeNode B) {
+
+	}
+}
