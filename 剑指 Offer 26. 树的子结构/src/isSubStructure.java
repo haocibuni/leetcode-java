695ccb18fbadeeb463c68a7fbcdbc68bb368edec
haocibuni <1723541415@qq.com>
2022-07-05 23:35:59 +0800

    issubstructure

--- 剑指 Offer 26. 树的子结构/src/isSubStructure.java
+++ 剑指 Offer 26. 树的子结构/src/isSubStructure.java
@@ -7,7 +7,7 @@ import javax.swing.tree.TreeNode;
 //深度遍历法 时间复杂度n*m
 //先对A进行遍历，然后判断A的子串和B是否匹配
 //需要注意树的结构也影响匹配
-class Solution {
+class Solution_DFS1 {
 	boolean flag = false;
 
 	public boolean isSubStructure(TreeNode A, TreeNode B) {
@@ -43,8 +43,12 @@ class Solution {
 	}
 }
 
-class Solution {
+//深度遍历法 时间复杂度n*m
+class Solution_DFS2 {
 	public boolean isSubStructure(TreeNode A, TreeNode B) {
+//		recur代表上一个方法的subDFS函数 与 左子树 右子树进行||运算
+//		需要添加B为空的特殊情况
+//		A为空是则进行DFS的回溯
 		return (A != null && B != null) && (recur(A, B) || isSubStructure(A.left, B) || isSubStructure(A.right, B));
 	}
 
