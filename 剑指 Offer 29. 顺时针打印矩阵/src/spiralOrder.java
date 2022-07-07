3f2c719e7c7a9b1dae5fc1d21164f4b5119744b7
haocibuni <1723541415@qq.com>
2022-07-07 12:28:32 +0800

    commit

--- 剑指 Offer 29. 顺时针打印矩阵/src/spiralOrder.java
+++ 剑指 Offer 29. 顺时针打印矩阵/src/spiralOrder.java
@@ -1,4 +1,6 @@
-
+//模拟边界法 时间复杂度mn 空间复杂度1
+//通过设置上下左右边界来进行有规律的遍历
+//遍历规则为从左往右，从上往下，从右往左，从下网上
 class Solution {
 	public int[] spiralOrder(int[][] matrix) {
 		if ((matrix == null || matrix.length == 0) || (matrix.length == 1 && matrix[0] == null)) {
@@ -11,46 +13,37 @@ class Solution {
 		int left = 0;
 		int[] res = new int[matrix.length * matrix[0].length];
 
-		int i = 0, j = 0, k = 0;
+		int k = 0;
+//		不能在while设置终止条件 需要每次遍历结束后对结果进行判断
 		while (true) {
 
-			while (j <= right) {
-				res[k++] = matrix[i][j++];
-
+			for (int i = left; i <= right; i++) {
+				res[k++] = matrix[down][i];
 			}
-			j--;
 			down = down + 1;
 			if (down > top) {
 				break;
-
 			}
-			while (i <= top) {
-				res[k++] = matrix[i++][j];
-
+			for (int i = down; i <= top; i++) {
+				res[k++] = matrix[i][right];
 			}
-			i--;
 			right = right - 1;
-			if (right > left) {
+			if (right < left) {
 				break;
-
 			}
-			while (j >= left) {
-				res[k++] = matrix[i][j--];
+			for (int i = right; i >= left; i--) {
+				res[k++] = matrix[top][i];
 			}
-			j++;
 			top = top - 1;
 			if (down > top) {
 				break;
-
 			}
-			while (i >= down) {
-				res[k++] = matrix[i--][j];
+			for (int i = top; i >= down; i--) {
+				res[k++] = matrix[i][left];
 			}
-			i++;
 			left = left + 1;
-			if (right > left) {
+			if (right < left) {
 				break;
-
 			}
 
 		}
